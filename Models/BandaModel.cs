9f54ac34bb99dc8cd3f8ad47cee37a3317bfc848
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-09-01 11:47:29 -0300

    feat: add BandaModel

---
+++ Models/BandaModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exercicios.Models
+{
+    [Table("Banda")]
+    public class BandaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int GeneroMusical { get; set; }
+
+        public bool Solo { get; set; }
+    }
+}
