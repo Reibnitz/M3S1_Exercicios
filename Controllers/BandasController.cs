281969bd18775ea6b7a268ef8d3e58982bf8d7bd
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-09-01 11:44:15 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/BandasController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercicios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandasController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
