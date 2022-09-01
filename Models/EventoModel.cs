c1efed54aca55eceba2c6f616ef91ed15bd4bb6f
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-09-01 11:50:28 -0300

    feat: add EventoModel

---
+++ Models/EventoModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Exercicios.Models
+{
+    [Table("Evento")]
+    public class EventoModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal CapacidadePublico { get; set; }
+
+        public DateTime Data { get; set; }
+
+        [Required]
+        [ForeignKey("Banda")]
+        public int BandaId { get; set; }
+
+        public BandaModel? Banda { get; set; }
+    }
+}
